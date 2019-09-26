f46d568a714ea75901b0a2a243273e6c0ca4e90e
John Baublitz <john.m.baublitz@gmail.com>
2019-09-25 22:48:55 -0400

    Work on stream module

--- src/lib.rs
+++ src/lib.rs
@@ -78,6 +78,8 @@ pub mod nlattr;
 pub mod rtnl;
 /// Wrapper for `libc` sockets
 pub mod socket;
+/// Module for high level stream interface
+pub mod stream;
 
 use std::ffi::CString;
 use std::io::{Read, Write};
